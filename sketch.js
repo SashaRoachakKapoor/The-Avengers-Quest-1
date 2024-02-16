4b5deaec9b4dabf5bd2cc78983b179d7d5a4c869
SashaRoachakKapoor <121806975+SashaRoachakKapoor@users.noreply.github.com>
2024-02-16 17:05:02 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var splashscreen
+var playbutton,aboutbutton
+
+
+function preload(){
+splashscreen=loadImage('assets/The quest.gif')
+}
+
+function setup(){
+createCanvas(windowWidth, windowHeight)
+
+}
+
+
+function draw(){
+background(splashscreen)
+
+    
+}
